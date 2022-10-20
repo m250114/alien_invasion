959adb1e1fc1b41f7c27422fee04cab219f73bf7
m250114 <115736634+m250114@users.noreply.github.com>
2022-10-19 20:53:22 -0400

    19OCT22 Bancroft

--- game.py
+++ game.py
@@ -12,16 +12,42 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
-        """"""
-    def run_game(self):
-        #Redraw the screen during each pass through the loop.
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-
-    #Make the most recently drawn screen visible.
-        pygame.display.flip()
+        """Start the main loop for the game."""
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type== pygame.QUIT:
+                    sys.exit()
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
 if __name__== '__main__':
-    #Make a game instance, and run the game.
-ai=AlienInvasion()
+    #make a game instance, and run the game.
+    ai= AlienInvasion()
+    ai.run_game()
+
+    #Set Background Music
+    self.bg_color=(230,230,230)
+
+    #redraw the screen during each pass through the loop.
+    self.screen.fill(self.bg_color)
+
+class Settings:
+    """A class to store all settings for alien invasions"""
+
+    def ___init___(self):
+        """Initialize the game's settings"""
+        #screen settings
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
+
+    from settings import Settings
+    self.settings=Settings()
+
+    self.screen.fill(self.settigns.bg_color)
+
+
+
+
 
