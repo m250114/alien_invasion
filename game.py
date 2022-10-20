5b80c9952b172ee116246cfd2395717278802f40
m250114 <115736634+m250114@users.noreply.github.com>
2022-10-20 08:42:12 -0400

    final lab 7

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
-import pygame
 import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -7,39 +11,38 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Watch for keyboard and mouse events.
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
-                if event.type== pygame.QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-if __name__== '__main__':
-    #make a game instance, and run the game.
-    ai= AlienInvasion()
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
     ai.run_game()
 
-    #Set Background Music
-    self.bg_color=(230,230,230)
 
-    #redraw the screen during each pass through the loop.
-    self.screen.fill(self.bg_color)
 
-from settings import Settings
-self.settings=Settings()
-self.screen.fill(self.settings.bg_color)
 
-from ship import Ship
-
-self.ship= Ship(self)
-self.ship.blitme()
 
 
 
--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 class Settings:
-    """A class to store all settings for alien invasions"""
+    """A class to store all settings for Alien Invasion."""
 
-    def ___init___(self):
-        """Initialize the game's settings"""
-        #screen settings
-        self.screen_width=1200
-        self.screen_height=800
-        self.bg_color=(230,230,230)
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 900
+        self.screen_height = 400
+        self.bg_color = (230, 230, 230)
